38d8dbb2867644928d8be1537bfb630ada151858
Davi S. Bento <davisilva4222@gmail.com>
2021-12-09 19:40:55 -0300

    hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  solidity: "0.8.4"
+};
